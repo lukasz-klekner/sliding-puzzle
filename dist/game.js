00999f90fc7a29a825a41279e8bc33f5e0140ebc
Luki Klekner <lukaszklekner@gmail.com>
2023-07-27 08:55:37 +0200

    feat: implements logic for changning tiles' postion

--- dist/game.js
+++ dist/game.js
@@ -1,1 +1,41 @@
-console.log("ts działa?");
+const tiles = document.querySelectorAll(".tile");
+const gameBoard = document.querySelector("#game-board");
+const gameState = [
+    [tiles[0], tiles[1], tiles[2]],
+    [tiles[3], tiles[4], tiles[5]],
+    [tiles[6], tiles[7], tiles[8]],
+];
+const render = (gameBoard, gameState) => {
+    gameState.forEach((row) => {
+        row.forEach((col) => {
+            gameBoard.appendChild(col);
+        });
+    });
+};
+gameBoard.addEventListener("click", (event) => {
+    const target = event.target;
+    let x, y;
+    gameState.forEach((row, rowIndex) => {
+        row.forEach((col, colIndex) => {
+            if (col === target) {
+                x = rowIndex;
+                y = colIndex;
+            }
+        });
+    });
+    let emptyX, emptyY;
+    gameState.forEach((row, rowIndex) => {
+        row.forEach((col, colIndex) => {
+            if (col.innerHTML === "") {
+                emptyX = rowIndex;
+                emptyY = colIndex;
+            }
+        });
+    });
+    if (y === emptyY && (x + 1 === emptyX || x - 1 === emptyX) || x === emptyX && (y + 1 === emptyY || y - 1 === emptyY)) {
+        const temp = gameState[x][y];
+        gameState[x][y] = gameState[emptyX][emptyY];
+        gameState[emptyX][emptyY] = temp;
+    }
+    render(gameBoard, gameState);
+});
--- game.ts
+++ game.ts
@@ -1,1 +1,49 @@
-console.log("ts działa?");
+const tiles: NodeListOf<HTMLDivElement> = document.querySelectorAll(".tile");
+const gameBoard: HTMLDivElement = document.querySelector("#game-board");
+
+const gameState = [
+  [tiles[0], tiles[1], tiles[2]],
+  [tiles[3], tiles[4], tiles[5]],
+  [tiles[6], tiles[7], tiles[8]],
+];
+
+const render = (gameBoard: HTMLDivElement, gameState: HTMLDivElement[][]) => {
+  gameState.forEach((row) => {
+    row.forEach((col) => {
+      gameBoard.appendChild(col);
+    });
+  });
+};
+
+gameBoard.addEventListener("click", (event: Event) => {
+  const target = event.target;
+
+  let x: number, y: number;
+
+  gameState.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      if (col === target) {
+        x = rowIndex;
+        y = colIndex;
+      }
+    });
+  });
+
+  let emptyX: number, emptyY: number;
+  gameState.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      if (col.innerHTML === "") {
+        emptyX = rowIndex;
+        emptyY = colIndex;
+      }
+    });
+  });
+
+  if (y === emptyY && (x + 1 === emptyX || x - 1 === emptyX) || x === emptyX && (y + 1 === emptyY || y - 1 === emptyY)) {
+    const temp = gameState[x][y];
+    gameState[x][y] = gameState[emptyX][emptyY];
+    gameState[emptyX][emptyY] = temp;
+  }
+
+  render(gameBoard, gameState);
+});
